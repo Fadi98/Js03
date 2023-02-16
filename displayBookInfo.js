138e2dcbd3b1f8460863447cab8e0734c7e5fb98
fadilatou <fadilatouhamamarou@gmail.com>
2023-02-16 12:50:35 +0100

    projet Js03

---
+++ displayBookInfo.js
@@ -0,0 +1,12 @@
+let book = {
+    Titre: "L'Enfant Noir",
+    Auteur: "Camara Laye",
+    année: 1953,
+}
+
+function displayBookInfo () {
+    console.log(`Titre: ${book.Titre}`);
+    console.log(`Auteur: ${book.Auteur}`);
+    console.log(`année: ${book.année}`);
+}
+displayBookInfo();
